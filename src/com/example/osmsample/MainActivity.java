617e97d0265c5c84b5ef58dbaa24ad6257681ad8
Taro Matsuzawa <taro@georepublic.co.jp>
2013-06-22 10:25:34 +0900

    サーバから取得を追加

--- src/com/example/osmsample/MainActivity.java
+++ src/com/example/osmsample/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.osmsample;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
@@ -13,6 +21,7 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -30,9 +39,7 @@ public class MainActivity extends Activity {
         this.mapView.getController().setZoom(17);
         this.mapView.getController().setCenter(new GeoPoint(35.64483, 139.40881));
         setContentView(mapView);
-        GeoPoint overlayPoint = new GeoPoint(35.64483, 139.40881);
-        ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
-        overlays.add(new OverlayItem("明星大学", "OSCの会場だよ", overlayPoint));
+        ArrayList<OverlayItem> overlays = getOverlays();
         ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
 		OnItemGestureListener<OverlayItem> gesture = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
 			@Override
@@ -66,5 +73,54 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private ArrayList<OverlayItem> getOverlays() {
+        ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
+        HttpClient httpClient = new DefaultHttpClient();
+        
+        StringBuilder uri = new StringBuilder("http://10.0.2.2:3000/maps.json");
+        HttpGet request = new HttpGet(uri.toString());
+        HttpResponse httpResponse = null;
+         
+        try {
+            httpResponse = httpClient.execute(request);
+        } catch (Exception e) {
+            Log.d("MainActivity", "Error HTTP Execute");
+            return overlays;
+        }
+         
+        int status = httpResponse.getStatusLine().getStatusCode();
+         
+        JSONArray rootObject = null;
+        if (HttpStatus.SC_OK == status) {
+            try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                httpResponse.getEntity().writeTo(outputStream);
+                String data = outputStream.toString();
+                rootObject = new JSONArray(data);
+            } catch (Exception e) {
+            	Log.e("MainActivity", "rootObject", e);
+            }
+        } else {
+            Log.d("MainActivity", "Status" + status);
+            return overlays;
+        }
+        if (rootObject == null) {
+        	Log.d("MainActivity", "rootObject is null");
+        	return overlays;
+        }
+        try {
+        	for (int i = 0; i < rootObject.length(); i++) {
+        		JSONObject obj = rootObject.getJSONObject(i);
+        		double latitude = obj.getDouble("latitude");
+        		double longitude = obj.getDouble("longitude");
+                GeoPoint overlayPoint = new GeoPoint(latitude, longitude);
+                String title = obj.getString("title");
+                overlays.add(new OverlayItem(title, title, overlayPoint));        		
+        	}
+        } catch (Exception e) {
+        	Log.d("MainActivity", "jsonError");
+        }
+        return overlays;
+	}
 	
 }
